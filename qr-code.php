628601d52d9d12a717fe9697c09f1359a3afa080
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-09-24 21:34:50 -0300

    Validando data

--- qr-code.php
+++ qr-code.php
@@ -12,21 +12,24 @@ $data = $_POST['data-de-nascimento'];
 $emailValido = v::email()->validate($email);
 $dataValida = v::date('d-m-Y')->validate($data);
 
-if ($emailValido) {
-	echo 'E-mail válido!';
-} else {
-	echo 'E-mail inválido!';
+// if ($emailValido) {
+// 	echo 'E-mail válido!';
+// } else {
+// 	echo 'E-mail inválido!';
+// }
+
+// if ($dataValida) {
+// 	echo '<br> Data válida';
+// } else {
+// 	echo '<br> Data inválida';
+// }
+
+
+
+if ($dataValida && $emailValido) {
+
+	$qrCode = new QrCode($nome);
+
+	header('Content-Type: '.$qrCode->getContentType());
+	echo $qrCode->writeString();
 }
-
-if ($dataValida) {
-	echo '<br> Data válida';
-} else {
-	echo '<br> Data inválida';
-}
-
-// if  $dataValida ? echo 'Data válida!': echo 'Data inválida';
-
-// $qrCode = new QrCode($nome);
-
-// header('Content-Type: '.$qrCode->getContentType());
-// echo $qrCode->writeString();
